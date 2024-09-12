1712eefb09e33ab650566c6575f39f9484e18ecf
Ubuntu <ubuntu@ip-172-31-65-83.ec2.internal>
2024-09-12 15:50:10 +0000

    Update

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import './App.css';
 
+const axios = require('axios');
 
 function App() {
   // Estado para los campos del formulario
