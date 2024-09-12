da21e334654d7381618930ed1357795a385a506d
Ubuntu <ubuntu@ip-172-31-65-83.ec2.internal>
2024-09-12 15:22:55 +0000

    Uodate app-test.js

--- app-react/src/App.test.js
+++ app-react/src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders registration form', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const registerButton = screen.getByText(/Registrar Usuario/i);
+  expect(registerButton).toBeInTheDocument();
+});
+
+test('submits the form and adds a user', async () => {
+  render(<App />);
+  const firstNameInput = screen.getByLabelText(/Nombres:/i);
+  const lastNameInput = screen.getByLabelText(/Apellidos:/i);
+  const birthDateInput = screen.getByLabelText(/Fecha de nacimiento:/i);
+  const passwordInput = screen.getByLabelText(/Password:/i);
+  const submitButton = screen.getByText(/Registrar/i);
+
+  fireEvent.change(firstNameInput, { target: { value: 'John' } });
+  fireEvent.change(lastNameInput, { target: { value: 'Doe' } });
+  fireEvent.change(birthDateInput, { target: { value: '1990-01-01' } });
+  fireEvent.change(passwordInput, { target: { value: 'password' } });
+
+  fireEvent.click(submitButton);
+
+  const user = await screen.findByText(/John Doe/i);
+  expect(user).toBeInTheDocument();
 });
